0a2d3b2d957acb54825f79a225dc3f077d4db662
akrck02 <aketza.vazquez@gmail.com>
2023-07-02 19:55:05 +0200

    [contacts] adding social media

--- frontend/src/views/contacts/ContactsView.ui.ts
+++ frontend/src/views/contacts/ContactsView.ui.ts
@@ -1,30 +1,59 @@
-import { UIComponent } from "../../lib/gtd/web/uicomponent.js";
+import { UIComponent, setStyles } from "../../lib/gtd/web/uicomponent.js";
 import { ViewUI } from "../../lib/gtdf/views/ViewUI.js";
+import { getSocialIcon } from "../../lib/socialicons.js";
 
 export class ContactsView extends ViewUI {
 
     private static ID = "contacts";
-    private static INFO_TEXT_ID = "info-text";
+    private static BOX_CLASS = "social-box";
+    
 
     constructor(){
         super({
             type: "view",
-            id: "contacts",
+            id: ContactsView.ID,
             classes: ["box-column","box-center"],
         });
     }
 
 
     public async show(params : string[], container : UIComponent): Promise<void> {
-        const contact = new UIComponent({
-            type: "p",
-            id: ContactsView.INFO_TEXT_ID,
-            text: "Contact me at: @skylerie",
-        });
+ 
+        this.createSocialBox("twitter","Skyleriearts","https://twitter.com/Skyleriearts");
+        this.createSocialBox("instagram","Skyleriie","https://www.instagram.com/skyleriie/");
+        this.createSocialBox("telegram","SkylerieArt","https://t.me/skylerie");
 
-        contact.appendTo(this);
         this.appendTo(container);
     }
 
+    createSocialBox(icon : string, text : string, link : string){
+
+        const twitterBox = new UIComponent({
+            type: "a",
+            classes: ["box-row", "box-center", "box-x-start", ContactsView.BOX_CLASS],
+            attributes: {
+                href: link,
+                target: "_blank",
+            },
+        });
+
+        const iconComponent = getSocialIcon(icon, {
+            fill: "white",
+            size: "2.5rem",
+        });
+
+        setStyles(iconComponent.element, {
+            margin: "0 .5rem",
+        });
+
+        const textComponent = new UIComponent({
+            type: "p",
+            text: text
+        });
+
+        iconComponent.appendTo(twitterBox);
+        textComponent.appendTo(twitterBox);
+        twitterBox.appendTo(this);
+    }
 
 }
