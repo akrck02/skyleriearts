0e0f6a0e7685fca6695a5be7e797728220683c39
akrck02 <43274508+akrck02@users.noreply.github.com>
2023-07-02 17:55:34 +0000

    Deploying to gh-pages from @ akrck02/SkylerieWebsite@0a2d3b2d957acb54825f79a225dc3f077d4db662 🚀

--- frontend/temp/src/views/contacts/ContactsView.ui.js
+++ frontend/temp/src/views/contacts/ContactsView.ui.js
@@ -1,22 +1,44 @@
-import { UIComponent } from "../../lib/gtd/web/uicomponent.js";
+import { UIComponent, setStyles } from "../../lib/gtd/web/uicomponent.js";
 import { ViewUI } from "../../lib/gtdf/views/ViewUI.js";
+import { getSocialIcon } from "../../lib/socialicons.js";
 export class ContactsView extends ViewUI {
     constructor() {
         super({
             type: "view",
-            id: "contacts",
+            id: ContactsView.ID,
             classes: ["box-column", "box-center"],
         });
     }
     async show(params, container) {
-        const contact = new UIComponent({
-            type: "p",
-            id: ContactsView.INFO_TEXT_ID,
-            text: "Contact me at: @skylerie",
-        });
-        contact.appendTo(this);
+        this.createSocialBox("twitter", "Skyleriearts", "https://twitter.com/Skyleriearts");
+        this.createSocialBox("instagram", "Skyleriie", "https://www.instagram.com/skyleriie/");
+        this.createSocialBox("telegram", "SkylerieArt", "https://t.me/skylerie");
         this.appendTo(container);
+    }
+    createSocialBox(icon, text, link) {
+        const twitterBox = new UIComponent({
+            type: "a",
+            classes: ["box-row", "box-center", "box-x-start", ContactsView.BOX_CLASS],
+            attributes: {
+                href: link,
+                target: "_blank",
+            },
+        });
+        const iconComponent = getSocialIcon(icon, {
+            fill: "white",
+            size: "2.5rem",
+        });
+        setStyles(iconComponent.element, {
+            margin: "0 .5rem",
+        });
+        const textComponent = new UIComponent({
+            type: "p",
+            text: text
+        });
+        iconComponent.appendTo(twitterBox);
+        textComponent.appendTo(twitterBox);
+        twitterBox.appendTo(this);
     }
 }
 ContactsView.ID = "contacts";
-ContactsView.INFO_TEXT_ID = "info-text";
+ContactsView.BOX_CLASS = "social-box";
